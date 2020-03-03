f71164fded17d1a1158878f6383e1ea493bb1c3e
Oscar Orozco <allmithy@gmail.com>
2020-03-02 22:16:46 -0600

    Creé el test de login

---
+++ src/saucedemo/tests/Login.tests.js
@@ -0,0 +1,13 @@
+import LoginPage from "../pages/Login.page"
+
+const STANDAR_USER = process.env.SAUCEDEMO_STANDAR_USER;
+const LOCKED_OUT_USER = process.env.SAUCEDEMO_LOCKED_OUT_USER;
+const PROBLEM_USER = process.env.SAUCEDEMO_PROBLEM_USER;
+const PERFORMANCE_GLITCH_USER = process.env.SAUCEDEMO_PERFORMANCE_GLITCH_USER;
+const PASSWORD = process.env.SAUCEDEMO_GENERAL_PASSWORD;
+
+fixture("Login Tests").page("https://www.saucedemo.com/");
+
+test("Login with a valid user", async t => {
+    await LoginPage.loginUser(STANDAR_USER, PASSWORD);
+});
