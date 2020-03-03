63fc41f0a30c7eac505a3f312064dc0138848650
Oscar Orozco <allmithy@gmail.com>
2020-03-02 22:16:35 -0600

    Agregué el metodo de login

--- src/saucedemo/pages/Login.page.js
+++ src/saucedemo/pages/Login.page.js
@@ -1,10 +1,18 @@
 import { Selector, t } from "testcafe";
 
 class LoginPage {
-    constructor(){
+    constructor() {
         //Login elements section
         this.userNameField = Selector("#user-name");
         this.passwordField = Selector("#password");
         this.loginButton = Selector("input[type=submit]");
     }
+
+    async loginUser(userName, password) {
+        await t.typeText(this.userNameField, userName)
+            .typeText(this.passwordField, password)
+            .click(this.loginButton);
+    }
 }
+
+export default new LoginPage();
